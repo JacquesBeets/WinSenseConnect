45792ec3df6b071bbb1ebc9c5d4ade730b75d942
Jacques Beets <jacques.beets@gmail.com>
2024-10-01 22:45:49 +0200

    some layout updates

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hidden md:block">
+    <!-- <Menu /> -->
+    <div class="border-t">
+      <div class="bg-background">
+        <div class="grid lg:grid-cols-5">
+          <!-- <Sidebar :playlists="playlists" class="hidden lg:block" /> -->
+          <div class="col-span-3 lg:col-span-4 lg:border-l">
+            <div class="h-full px-4 py-6 lg:px-8">
+              <slot />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+// import Menu from './components/Menu.vue'
+// import Sidebar from './components/Sidebar.vue'
+</script>
