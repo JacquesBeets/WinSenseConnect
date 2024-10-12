08655f87f2596b6e626e3cee64dbf3938b118f93
Jacques Beets <jacques.beets@gmail.com>
2024-10-12 17:20:28 +0200

    small adjustments

--- frontend/pages/config/scripts/[id].vue
+++ frontend/pages/config/scripts/[id].vue
@@ -5,22 +5,22 @@
       <input type="text" id="scriptId" v-model="script.id" disabled/>
     </div>
     <div class="form-control">
-      <label for="name">Name</label>
+      <label for="name">MQTT Command Name</label>
       <input type="text" id="name" v-model="script.name" />
     </div>
     <div class="form-control">
       <label for="scriptPath">Script Path</label>
-      <input type="text" id="scriptPath" v-model="script.script_path" />
-    </div>
-    <div class="form-control">
-      <div class="flex items-center me-6">
-      <input v-model="script.run_as_user" checked id="primary-checkbox" type="checkbox" value="" class="w-6 h-6 text-primary-400 bg-gray-100 border-gray-300 rounded focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-      <label for="primary-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300" >Run as User</label>
-    </div>
+      <input type="text" id="scriptPath" disabled v-model="script.script_path" />
     </div>
     <div class="form-control">
       <label for="scriptTimeout">Script Timeout</label>
       <input type="number" id="scriptTimeout" v-model="script.script_timeout" />
+    </div>
+    <div class="form-control mt-6">
+      <div class="flex items-center me-6">
+        <input v-model="script.run_as_user" checked id="primary-checkbox" type="checkbox" value="" class="w-6 h-6 text-primary-400 bg-gray-100 border-gray-300 rounded focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+        <label for="primary-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300" >Run as User</label>
+      </div>
     </div>
     <div class="form-control">
       <button @click.stop="saveConfig" class="btn-primary ml-auto" :disabled="isSaving">
